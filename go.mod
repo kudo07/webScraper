b39a77c0ca00731f640032df6df4390a6cc5baf2
kudo07 <vknwl077@gmail.com>
2025-12-14 23:31:58 +0530

    feat:intiliasationss

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kudo07/webScraper
 
 go 1.23.0
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found")
+	}
+	fmt.Println("PORT:", portString)
 }
